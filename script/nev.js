ca60c895eefad90a6787292e6d2506c9fb43c5b9
Randy Gingeleski <rgingeleski@aol.com>
2015-06-24 16:30:25 -0400

    fixed text chat box with new js names

--- script/nev.js
+++ script/nev.js
@@ -1,40 +1,40 @@
-var eliza = new ElizaBot();
-var elizaLines = new Array();
+var nev = new NevBot();
+var nevLines = new Array();
 
 var displayCols = 60;
 var displayRows = 20;
 
-function elizaReset() {
-	eliza.reset();
-	elizaLines.length = 0;
-	elizaStep();
+function nevReset() {
+	nev.reset();
+	nevLines.length = 0;
+	nevStep();
 }
 
-function elizaStep() {
+function nevStep() {
 	var f = document.forms.e_form;
 	var userinput = f.e_input.value;
-	if (eliza.quit) {
+	if (nev.quit) {
 		f.e_input.value = '';
-		if (confirm("This session is over.\nStart over?")) elizaReset();
+		if (confirm("This session is over.\nStart over?")) nevReset();
 		f.e_input.focus();
 		return;
 	}
 	else if (userinput != '') {
 		var usr = 'YOU:   ' + userinput;
-		var rpl ='NEV: ' + eliza.transform(userinput);
-		elizaLines.push(usr);
-		elizaLines.push(rpl);
+		var rpl ='NEV: ' + nev.transform(userinput);
+		nevLines.push(usr);
+		nevLines.push(rpl);
 		// display nicely
 		// (fit to textarea with last line free - reserved for extra line caused by word wrap)
 		var temp  = new Array();
 		var l = 0;
-		for (var i=elizaLines.length-1; i>=0; i--) {
-			l += 1 + Math.floor(elizaLines[i].length/displayCols);
+		for (var i=nevLines.length-1; i>=0; i--) {
+			l += 1 + Math.floor(nevLines[i].length/displayCols);
 			if (l >= displayRows) break
-			else temp.push(elizaLines[i]);
+			else temp.push(nevLines[i]);
 		}
-		elizaLines = temp.reverse();
-		f.e_display.value = elizaLines.join('\n');
+		nevLines = temp.reverse();
+		f.e_display.value = nevLines.join('\n');
 	}
 	f.e_input.value = '';
 	f.e_input.focus();
